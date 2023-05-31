be19383416182d6652ef09536e2e80d2637f2b01
Alberto-Knox <volkankarakaya2020@gmail.com>
2023-06-01 01:20:56 +0300

    add remove and edit item functions

--- src/Items.jsx
+++ src/Items.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import SingleItem from './SingleItem'
 import { nanoid } from 'nanoid'
 
-const Items = ({items,removeItem}) => {
+const Items = ({items,removeItem, editItem}) => {
   return (
     <div className="items">
         {items.map((item) => {
-            return <SingleItem item={item} key={nanoid()} removeItem={removeItem}/>
+            return <SingleItem item={item} key={nanoid()} removeItem={removeItem} editItem={editItem}/>
         })}
     </div>
   )
--- src/SingleItem.jsx
+++ src/SingleItem.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react'
 
-const SingleItem = ({item, removeItem}) => {
-    const [isChecked,setIsChecked] = useState(item.completed)
+const SingleItem = ({item, removeItem, editItem}) => {
+    
   return (
     <div className="single-item">
-        <input type="checkbox" checked={isChecked} onChange={() => setIsChecked(!isChecked)}/>
-        <p style={{textTransform: 'capitalize', textDecoration:isChecked && 'line-through'}}>{item.name}</p>
+        <input type="checkbox" checked={item.completed} onChange={() => editItem(item.id)}/>
+        <p style={{textTransform: 'capitalize', textDecoration:item.completed && 'line-through'}}>{item.name}</p>
         <button className="btn remove-btn" onClick={() => {removeItem(item.id)}}>
             delete
         </button>
